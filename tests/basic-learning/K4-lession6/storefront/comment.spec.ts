6b3f3e22b652b5a0a796439cb382c37777a5578b
Tuando176 <hoangtuan1760@gmail.com>
2026-05-18 22:45:12 +0700

    update

---
+++ tests/basic-learning/K4-lession6/storefront/comment.spec.ts
@@ -0,0 +1,24 @@
+import test, { expect } from "@playwright/test";
+
+test(" add comment",async ({ page }) => {
+    // go to page https://shop.congcu.org/2024/02/25/kinh-nghiem-chon-va-thuong-thuc-bia-doc-dao-tren-toan-the-gioi/
+    await page.goto("https://shop.congcu.org/2024/02/25/kinh-nghiem-chon-va-thuong-thuc-bia-doc-dao-tren-toan-the-gioi/");
+
+    // add bình luận
+    await page.locator("//textarea[@id='comment']").fill("learning Playwright");
+
+    // add tên
+    await page.locator("//input[@id='author']").fill("tuando");
+
+    //add email
+    await page.locator("//input[@id='email']").fill("tdohoang@rbbn.com");
+
+    // add trang web
+
+    // click on phản hồi btn
+    await page.locator("//input[@id='submit']").click();
+
+    //verify cmt
+    const blockCmtLoc = page.locator("//em[@class='comment-awaiting-moderation']");
+    await expect(blockCmtLoc).toBeVisible();
+});
