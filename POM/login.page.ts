702754b2247f59ae3f5c24728bfdeee85af1dbcc
Tuando176 <hoangtuan1760@gmail.com>
2026-05-27 22:08:38 +0700

    lession8

--- POM/login.page.ts
+++ POM/login.page.ts
@@ -17,12 +17,12 @@ export class LoginPage {
     this.passwordLoc = page.locator("//input[@id='user_pass']")
     this.remberLoc = page.locator("//input[@id='rememberme']")
     this.loginLoc = page.locator("//input[@id='wp-submit']")
-  }
+  };
 
   // MEthod / function
   async gotoLogin() {
     await this.page.goto("https://shop.congcu.org/wp-login.php");
-  }
+  };
 
   async fillInformation(email: string, password: string, isRemember: boolean) {
     await this.emailLoc.fill(email);
@@ -30,23 +30,23 @@ export class LoginPage {
     if (isRemember) {
       await this.remberLoc.check();
     }
-  }
+  };
 
   async login() {
     await this.loginLoc.click();
-  }
+  };
 
   async performLogin(username: string, password: string, isRemember: boolean) {
     await this.gotoLogin();
     await this.fillInformation(username, password, isRemember);
     await this.login();
-  }
+  };
 
   async chooseLanguage(languageName: string) {
     // Select language with name
     await this.page.locator("//select[@id='language-switcher-locales']").selectOption({
       label: languageName,
     });
-  }
+  };
 
 }
